5f5cd44888a0e135c6c7ae9d4d6695d9c2f8b561
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-11-18 18:31:44 +0600

    email and password validation or six charecter and special charecter set previous email not allow alert validation

--- src/components/registerReactBootstrap.js
+++ src/components/registerReactBootstrap.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import app from "../firebase/firebase.init";
@@ -7,23 +7,46 @@ import app from "../firebase/firebase.init";
 const auth = getAuth(app);
 
 const RegisterReactBootstrap = () => {
+  const [passwordError, setPasswordError] = useState("");
+  const [success, setSuccess] = useState(false);
+
   const handleRegister = (event) => {
     event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-    console.log(email, password);
+    setSuccess(false);
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    // console.log(email, password);
+
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setPasswordError("Please Provide at least tow uppercase");
+      return;
+    }
+    if (password.length < 6) {
+      setPasswordError("Please should be at six characters.");
+      return;
+    }
+    if (!/(?=.*[!@$%&*])/.test(password)) {
+      setPasswordError("Please add at least one special characters");
+      return;
+    }
+    setPasswordError("");
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
         console.log(user);
+        setSuccess(true);
+        form.reset();
       })
       .catch((error) => {
         console.log("error", error);
+        setPasswordError(error.message);
       });
   };
   return (
-    <div className="w-50 mx-auto">
-      <h2 className="text-primary">Please Register !!!</h2>
+    <div className="w-50 mx-auto mt-5">
+      <h2 className="text-primary text-center">Please Register !!!</h2>
       <Form onSubmit={handleRegister}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
@@ -43,6 +66,9 @@ const RegisterReactBootstrap = () => {
             required
           />
         </Form.Group>
+        <p className="text-danger">{passwordError}</p>
+        {success && <p className="text-success">User Created SuccessFully</p>}
+
         <Button variant="primary" type="submit">
           Register
         </Button>
